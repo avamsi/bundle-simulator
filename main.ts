c76c1cb26a4f108ea4220d11c48e7d78797580fe
Vamsi Avula <vamsi_ism@outlook.com>
2022-11-22 16:33:59 +0530

    make weekly and (especially) daily more intelligent

--- main.ts
+++ main.ts
@@ -1,30 +1,42 @@
-function moveUnreadThreadsToInbox(label: string) {
-    var threads = GmailApp.search('is:unread label:' + label)
-    // For whatever reason, GmailApp.moveThreadsToInbox throws when asked
-    // to move more than 100 threads at once.
-    var maxThreads = 100
-    for (var i = 0; i < threads.length; i += maxThreads) {
-      GmailApp.moveThreadsToInbox(threads.slice(i, i + maxThreads))
-    }
-}
-
 function daily() {
-    moveUnreadThreadsToInbox('daily');
+	const threads = GmailApp.search('label:daily -in:inbox is:unread -is:muted');
+	if (threads.length === 0) {
+		return;
+	}
+	const labelIdsToRemove = [];
+	for (const label of Gmail.Users.Labels.list('me').labels) {
+		if (label.name == 'daily' || label.name == 'skipped') {
+			labelIdsToRemove.push(label.id);
+		}
+	}
+	for (const thread of threads) {
+		const labels = {
+			'addLabelIds': ['INBOX'],
+			'removeLabelIds': ['CATEGORY_UPDATES', ...labelIdsToRemove],
+		};
+		Gmail.Users.Threads.modify(labels, 'me', thread.getId());
+	}
 }
 
 function weekly() {
-    moveUnreadThreadsToInbox('weekly');
+	const threads = GmailApp.search('label:weekly -in:inbox is:unread -is:muted');
+	// For whatever reason, GmailApp.moveThreadsToInbox throws when asked to move
+	// more than 100 threads at once. Avoid that by buffering manually.
+	const maxThreads = 100;
+	for (let i = 0; i < threads.length; i += maxThreads) {
+		GmailApp.moveThreadsToInbox(threads.slice(i, i + maxThreads));
+	}
 }
 
 function scheduleTriggers() {
-    ScriptApp.newTrigger('daily')
-        .timeBased()
-        .atHour(8)
-        .everyDays(1)
-        .create();
-    ScriptApp.newTrigger('weekly')
-        .timeBased()
-        .atHour(11)
-        .onWeekDay(ScriptApp.WeekDay.SATURDAY)
-        .create();
+	ScriptApp.newTrigger('daily')
+		.timeBased()
+		.atHour(8)
+		.everyDays(1)
+		.create();
+	ScriptApp.newTrigger('weekly')
+		.timeBased()
+		.atHour(11)
+		.onWeekDay(ScriptApp.WeekDay.SATURDAY)
+		.create();
 }
